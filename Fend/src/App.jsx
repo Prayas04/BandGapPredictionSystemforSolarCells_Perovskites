b63088c7ab242dfd6a8a9dd0d8fdff7fbd9856e3
Prayas04 <prayaskamila2004@gmail.com>
2026-07-14 15:24:36 +0530

    fix: resolved page refresh error.

--- Fend/src/App.jsx
+++ Fend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Dataset from './pages/Dataset'
 import About from './pages/About'
